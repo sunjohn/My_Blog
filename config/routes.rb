fb5b2f23b4e884e6353518a689048265d113a813
sunjohn <song@rail.com>
2012-08-26 21:52:20 +0800

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 MyBlog::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/work"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
